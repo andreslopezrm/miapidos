426a7af331d5534c431d91e9bd4e4873c44f989a
Andres Lopez <andres_lopez_rm@hotmail.com>
2024-08-30 12:55:33 -0600

    Add files via upload

---
+++ api/index.ts
@@ -0,0 +1,79 @@
+import { OpenAPIHono, createRoute, z } from '@hono/zod-openapi'
+import { swaggerUI } from '@hono/swagger-ui'
+import { handle } from 'hono/vercel'
+
+const app = new OpenAPIHono().basePath('/api')
+
+app.openapi(
+  createRoute({
+    method: 'get',
+    path: '/hello',
+    responses: {
+      200: {
+        description: 'Respond a message',
+        content: {
+          'application/json': {
+            schema: z.object({
+              message: z.string()
+            })
+          }
+        }
+      }
+    }
+  }),
+  (c) => {
+    return c.json({
+      message: 'hello'
+    })
+  }
+)
+
+
+app.openapi(
+  createRoute({
+    method: 'get',
+    path: '/',
+    responses: {
+      200: {
+        description: 'Respond a message',
+        content: {
+          'application/json': {
+            schema: z.object({
+              status: z.number(),
+              message: z.string(),
+            })
+          }
+        }
+      }
+    }
+  }),
+  (c) => {
+    return c.json({
+      status: 200,
+      message: 'inicio'
+    })
+  }
+)
+
+app.get(
+  '/ui',
+  swaggerUI({
+    url: '/doc'
+  })
+)
+
+app.doc('/doc', {
+  info: {
+    title: 'An API',
+    version: 'v1'
+  },
+  openapi: '3.1.0'
+})
+
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
