e253b3cca58d75dffb724a7522ae777c91c4c94c
Andres Lopez <andres_lopez_rm@hotmail.com>
2024-09-04 12:29:39 -0600

    Add files via upload

--- server/todo.route.ts
+++ server/todo.route.ts
@@ -1,7 +1,7 @@
 import { OpenAPIHono, createRoute, z } from '@hono/zod-openapi'
 import { swaggerUI } from '@hono/swagger-ui'
 
-export const todos = new OpenAPIHono().basePath('/api')
+export const todos = new OpenAPIHono()
 
 todos.openapi(
   createRoute({
