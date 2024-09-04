e9afe417db71f5d9fcf27fe8df4b3891cb0d489a
Andres Lopez <andres_lopez_rm@hotmail.com>
2024-09-04 12:59:11 -0600

    Add files via upload

--- server/info.route.ts
+++ server/info.route.ts
@@ -1,4 +1,5 @@
 import { OpenAPIHono, createRoute, z } from '@hono/zod-openapi'
+import { HTTPException } from 'hono/http-exception'
 
 export const info = new OpenAPIHono()
 
@@ -14,19 +15,23 @@ info.openapi(
             schema: z.object({
               data: z.object({
                 id: z.number(),
-                name: z.string()
+                name: z.string(),
               })
             })
           }
         }
-      }
+      },
     }
   }),
   (c) => {
+
+    throw new HTTPException(401, { message: 'Custom error message for info' })
+    
     const data = {
         id: 1,
-      name: 'kal'
+      name: 'kal',
     }
+
     return c.json({ data })
   }
 )
